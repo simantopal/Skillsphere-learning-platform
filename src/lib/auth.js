11f2b296381a9978963bc80983376547dc7c84f0
simantopal <simantop13@gmail.com>
2026-05-03 00:54:39 +0600

    some change

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
-const db = client.db("Skillsphere");
+const db = client.db("Skillsphere-platform");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
